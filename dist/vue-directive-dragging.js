2c0e4565a263a235c0a358a86094f0e3c75a7ee4
dengwb <dengwenbin@wecash.net>
2019-08-18 17:12:39 +0800

    v0.0.2

--- dist/vue-directive-dragging.js
+++ dist/vue-directive-dragging.js
@@ -1,14 +1,66 @@
 /**
- * vue-directive-dragging v0.0.1
+ * vue-directive-dragging v0.0.2
  * (c) 2019 dengwb
  * @license MIT
  */
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.VueDirectiveDragging = factory());
 }(this, (function () { 'use strict';
 
-	console.log('Implementing a drag-and-drop plugin based on the vue directive');
+  var events = {
+    on: function on(el, type, fn) {
+      el.addEventListener(type, fn);
+    },
+    off: function off(el, type, fn) {
+      el.removeEventListener(type, fn);
+    }
+  };
+
+  function install(Vue, options) {
+    var _el = null;
+    var diffTop = null;
+    var diffLeft = null;
+
+    function handleDragStart(event) {
+      event.preventDefault();
+      diffLeft = event.touches[0].pageX - _el.offsetLeft;
+      diffTop = event.touches[0].pageY - _el.offsetTop;
+    }
+
+    function handleDragMove(event) {
+      var left = event.touches[0].pageX - diffLeft;
+      var top = event.touches[0].pageY - diffTop;
+
+      _el.style.left = left + 'px';
+      _el.style.top = top + 'px';
+    }
+    function saveDragItem(el, binding, vnode) {
+      _el = el;
+      events.on(el, 'touchstart', handleDragStart);
+      events.on(el, 'touchmove', handleDragMove);
+    }
+
+    function updateDragItem(el, binding, vnode) {}
+
+    function removeDragItem(el, binding, vnode) {
+      events.off(el, 'touchstart', handleDragStart);
+      events.off(el, 'touchmove', handleDragMove);
+    }
+
+    Vue.directive('dragging', {
+      bind: saveDragItem,
+      update: updateDragItem,
+      unbind: removeDragItem
+    });
+  }
+
+  var index = {
+    install: install,
+    version: '0.0.2'
+  };
+
+  return index;
 
 })));
